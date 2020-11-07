9721810b46d296355ba5c39c5e1e34b64843e317
David Jones <drj@sanger.ac.uk>
2020-11-07 13:14:36 +0000

    Remove list push no longer needed

--- src/ignore_reg_access.c
+++ src/ignore_reg_access.c
@@ -190,7 +190,6 @@ List *ignore_reg_access_resolve_ignores_to_analysis_sections(int start, int end,
         List_push(reg_for_analysis,range);
         fprintf(stderr,"Pushing region %d-%d\n",range->beg ,range->end);
     }
-    List_push(reg_for_analysis,range);
     List_clear_destroy(li);
     return reg_for_analysis;
 error:
